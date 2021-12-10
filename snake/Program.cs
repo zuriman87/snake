d59e368fef8daf6e5d762a09fbee6f01ae0ee4b2
zuriman87 <zuriman87@gmail.com>
2021-12-10 15:32:14 +0200

    Написали класс Список

--- snake/Program.cs
+++ snake/Program.cs
@@ -10,18 +10,55 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point();
-            p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';
+            Point p1 = new Point(1, 3, '*');
             p1.Draw();
 
-            Point p2 = new Point();
-            p2.x = 4;
-            p2.y = 5;
-            p2.sym = '#';
+            Point p2 = new Point(4, 5, '#');
             p2.Draw();
-            
+
+            Point p3 = new Point(7, 2, '&');
+            p3.Draw();
+
+            Point p4 = new Point(3, 7, '@');
+            p4.Draw();
+
+            List<int> numList = new List<int>();
+            numList.Add (0);
+            numList.Add (1);
+            numList.Add (2);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            foreach(int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            List<Point> pList = new List<Point>();
+
+            pList.Add (p1);
+            pList.Add (p2);
+            pList.Add (p3);
+            pList.Add (p4);
+
+            List<char> charList = new List<char> ();
+
+            charList.Add('*');
+            charList.Add('&');
+            charList.Add('^');
+            charList.Add('%');
+            charList.Add('$');
+            charList.Add('#');
+            charList.Add('@');
+            charList.Add('!');
+
+            foreach (char b in charList)
+            {
+                Console.WriteLine(b);
+            }
+
             Console.ReadLine();
         }
 
